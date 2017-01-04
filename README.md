e0ab5b6a3d9b9db1e75b2a15200eeae08ae77bb9
emmettmccann <mccannemmett@gmail.com>
2017-01-04 10:04:59 -0800

    Merge branch 'master' of https://github.com/luuuk/Tactus

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tactus
 Tactus
+link to H-bridge circuit: https://circuits.io/circuits/3483491-h-bridge-testing
--- arduino/Sketches/SlidePotClass/SlidePot.ino
+++ arduino/Sketches/SlidePotClass/SlidePot.ino
@@ -1,5 +1,41 @@
 class SlidePot {
-  
-  
+  private:
+    int pot;//pin for potentiometer
+    int en;//pin for 'enable' on H-bridge
+    int fr;//pin for forward motor
+    int bk;//pin for backward motor
+    int dest;//destination
+    
+  public:
+    SlidePot(int _pot, int _en, int _fr, int _bk);
+    void set(int pos);//set destination
+    int getPos();//get current pot position
+    int getForce();//amount of force exerted on motor; current draw
+    void check();//checks current position against destination and moves accordingly
+};
+
+SlidePot::SlidePot(int _pot, int _en, int _fr, int _ bk) {
+  pot = _pot;
+  en = _en;
+  fr = _fr;
+  bk = _bk;
 }
+
+void SlidePot::set(int pos) {
+  dest = pos;
+}
+
+void SlidePot::getPos() {
+  return analogRead(pot);
+}
+
+void SlidePot::getForce() {
+  //TODO
+}
+
+void SlidePot::check() {
+  //TODO
+}
+
+
 
