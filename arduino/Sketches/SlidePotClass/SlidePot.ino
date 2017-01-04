9e9199337d514ed4d6ba002e961fab2c1c370507
EtherealFog <baldwinelias77@gmail.com>
2017-01-04 09:45:40 -0800

    working on check method

--- arduino/Sketches/SlidePotClass/SlidePot.ino
+++ arduino/Sketches/SlidePotClass/SlidePot.ino
@@ -1,24 +1,27 @@
+const int margin = 5;//acceptable margin of error for slidepot positioning
 class SlidePot {
   private:
     int pot;//pin for potentiometer
     int en;//pin for 'enable' on H-bridge
     int fr;//pin for forward motor
     int bk;//pin for backward motor
+    int force; //pin for force sensing (current draw)
     int dest;//destination
     
   public:
-    SlidePot(int _pot, int _en, int _fr, int _bk);
+    SlidePot(int _pot, int _en, int _fr, int _bk, int _force);
     void set(int pos);//set destination
     int getPos();//get current pot position
     int getForce();//amount of force exerted on motor; current draw
     void check();//checks current position against destination and moves accordingly
 };
 
-SlidePot::SlidePot(int _pot, int _en, int _fr, int _ bk) {
+SlidePot::SlidePot(int _pot, int _en, int _fr, int _ bk, int _force) {
   pot = _pot;
   en = _en;
   fr = _fr;
   bk = _bk;
+  force = _force;
 }
 
 void SlidePot::set(int pos) {
@@ -30,11 +33,15 @@ void SlidePot::getPos() {
 }
 
 void SlidePot::getForce() {
-  //TODO
+  return analogRead(force);//this is just how I imagine this will work; 
+                           //there might need to be some mapping/calculations added.
 }
 
 void SlidePot::check() {
-  //TODO
+  int potpos = analogRead(pot);
+  int diff = potpos-dest;//diff between desired position and actual position
+  if(abs(diff) <= margin) return;//if diff is within acceptable margin, return; no adjustment needed.
+  
 }
 
 
