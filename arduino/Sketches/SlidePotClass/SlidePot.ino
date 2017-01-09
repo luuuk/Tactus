4849ee76a612583267d26cf93ffba4df126f5144
EtherealFog <baldwinelias77@gmail.com>
2017-01-09 09:05:09 -0800

    changed to use Adafruit Motorshield library for motor control

--- arduino/Sketches/SlidePotClass/SlidePot.ino
+++ arduino/Sketches/SlidePotClass/SlidePot.ino
@@ -1,11 +1,20 @@
+//might need to have the following imports in file using this class as well.
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
 const int margin = 5;//acceptable margin of error for slidepot positioning
 const int pwm_speed = 127;//speed for moving slidepot motor. currently constant; may make dynamic later
 class SlidePot {
   private:
+    Adafruit_MotorShield AFMS;
+    Adafruit_DCMotor motor;
+  /*
     int pot;//pin for potentiometer (analog pin)
     int en;//pin for 'enable' on H-bridge (digital pwm pin)
     int fr;//pin for forward motor (digital pin)
     int bk;//pin for backward motor (digital pin)
+  */  
     int force; //pin for force sensing (current draw)
     int dest;//destination
     void mov(bool dir, int pwm);//sets motor speed in a given direction; (0 < pwm < 255)
@@ -21,12 +30,11 @@ class SlidePot {
     void check();//checks current position against destination and moves accordingly
 };
 
-SlidePot::SlidePot(int _pot, int _en, int _fr, int _ bk, int _force) {
-  pot = _pot;
-  en = _en;
-  fr = _fr;
-  bk = _bk;
-  force = _force;
+SlidePot::SlidePot() {
+  AFMS = Adafruit_MotorShield();
+  motor = AFMS.getMotor(1);
+  AFMS.begin();
+  
 }
 
 void SlidePot::set(int pos) {
@@ -51,23 +59,15 @@ void SlidePot::check() {
 
 void SlidePot::mov(bool dir, int pwm) {
   if(dir) {
-    digitalWrite(fr, HIGH);
-    digitalWrite(bk, LOW);
+    motor.run(FORWARD);
   } else {
-    digitalWrite(bk, HIGH);
-    digitalWrite(fr, LOW);
+    motor.run(BACKWARD);
   }
-  analogWrite(en, pwm);
-}
-
-void SlidePot::stp() {
-  digitalWrite(en, HIGH);
-  digitalWrite(fr, LOW);
-  digitalWrite(bk, LOW);
+  motor.setSpeed(pwm);
 }
 
 void SlidePot::fre() {
-  digitalWrite(en, LOW);
+  motor.run(RELEASE);
 }
 
 
--- arduino/Sketches/SlidePotClass/SlidePotClass.ino
+++ arduino/Sketches/SlidePotClass/SlidePotClass.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_MotorShield.h>
+
 void setup() {
   // put your setup code here, to run once:
 
