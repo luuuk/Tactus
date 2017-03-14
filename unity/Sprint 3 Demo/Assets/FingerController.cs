1116b65c19e895d5e532721d2ce326df85192703
EtherealFog <baldwinelias77@gmail.com>
2017-03-14 09:01:22 -0700

    Added sprint 3 demo

---
+++ unity/Sprint 3 Demo/Assets/FingerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerController : MonoBehaviour {
+
+    public int ARDUINO_MAX_VALUE;
+    public GameObject knuckle;
+    public GameObject f0;
+    public GameObject f1;
+    public GameObject f2;
+    public GameObject[] segments;
+    private GloveSerial port;
+    public float fingerPos { get; set; }
+    public float knucklePos { get; set; }
+	//private GloveSerial port;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//port = new GloveSerial();
+        segments = new GameObject[3];
+        segments[0] = f0;
+        segments[1] = f1;
+        segments[2] = f2;
+        knucklePos = 20;
+        //port = new GloveSerial();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        //serial updates
+        //port.Check();
+        //fingerPos = port.Get(0);
+        //knucklePos = port.Get(1);
+        //position updates
+        //servoPos();
+		updatePos();
+        float torque = f1.GetComponent<HingeJoint>().currentTorque.magnitude;
+        if(torque != 0)
+        {
+            Debug.Log(torque*1000000);
+            int valToSend = (int)Mathf.Round(map(torque * 1000000, 0, 25, 0, ARDUINO_MAX_VALUE));
+            //uncomment the following to send torque data to the serial port (make sure to uncomment all other lines involving the object "port" as well)
+            //port.Set(0, valToSend);
+            //port.Send();
+        }
+	}
+
+    void servoPos()
+    {
+        port.Check();
+        int servopos = port.Get(0);
+        setFingerPos(map(servopos, 0, 180, 0, 110));
+    }
+
+    static float map(float val, float min1, float max1, float min2, float max2)
+    {
+        return (val - min1) * ((max2 - min2) / (max1 - min1)) + min2;
+    }
+
+    void updatePos()
+    {
+        setFingerPos(fingerPos);
+        setKnucklePos(knucklePos);
+    }
+
+    void setFingerPos(float val)
+    {
+        HingeJoint hingeJoint = f0.GetComponent<HingeJoint>();
+        HingeJoint hingeJoint1 = f1.GetComponent<HingeJoint>();
+        JointSpring spring = hingeJoint.spring;
+        JointSpring spring1 = hingeJoint1.spring;
+        spring.targetPosition = -val;
+        spring1.targetPosition = -val;
+        hingeJoint.spring = spring;
+        hingeJoint1.spring = spring1;
+    }
+
+    void setKnucklePos(float val)
+    {
+        HingeJoint knuckleJoint = knuckle.GetComponent<HingeJoint>();
+        JointSpring kSpring = knuckleJoint.spring;
+        kSpring.targetPosition = -val + 20;
+        knuckleJoint.spring = kSpring;
+    }
+}
---
+++ unity/Sprint 3 Demo/Assets/GloveSerial.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.IO.Ports;
+using System.Collections.Generic;
+
+
+public class GloveSerial
+{
+    //private const string portName = "/dev/cu.usbmodem1421";
+    private const string portName = "COM3";
+    private Dictionary<int, int> map;
+    private SerialPort port;
+    private string buffer;
+	public int uno { get; set; }
+	public int dos { get; set; }
+
+    public GloveSerial()
+    {
+        port = new SerialPort(portName);
+        port.BaudRate = 9600;
+        port.Open();
+        map = new Dictionary<int, int>();
+        buffer = "";
+        Debug.Log("GloveSerial Created");
+    }
+     
+    public void Check()
+    {
+        string newline;
+		Debug.Log (port.IsOpen);
+        //while (port.BytesToRead > 0)
+        //{
+			Debug.Log ("Reading:");
+            newline = port.ReadLine();
+			Debug.Log (newline);
+            string[] pairs = newline.Split('&');
+            for (int i = 1; i < pairs.Length; i += 1)
+            {
+				Debug.Log (pairs [i]);
+				int colon = pairs [i].IndexOf (":");
+				int name = int.Parse (pairs [i].Substring (0, colon));
+				Debug.Log ("Name: " + name);
+				int val = int.Parse (pairs [i].Substring (colon + 1));
+				Debug.Log ("Val " + val);
+				map [name] = val;
+            }
+        //}
+		map [0] = uno;
+		map [1] = dos;
+    }
+
+    //adds a name-value pair to the buffer
+    public void Set(int key, int val)
+    {
+        buffer += "&" + key + ":" + val;
+    }
+
+	public void SlideSend(float val) {
+		port.WriteLine ("&0:" + (int)val + "&1:1&2:5");
+		Debug.Log ("Sending: " + "&0:" + val);
+	}
+    
+    //Must be called in the "Update" method of the object extending Monobehavior
+    public void Send()
+    {
+        port.WriteLine(buffer);
+        buffer = "";
+    }
+
+    public int Get(int key)
+    {
+        if (!map.ContainsKey(key))
+        {
+            Debug.LogError("Key does not exist");
+            return -1;
+        }
+        return map[key];
+    }
+}
+
